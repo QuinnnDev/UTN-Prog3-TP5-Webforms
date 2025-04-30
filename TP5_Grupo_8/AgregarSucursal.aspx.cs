cd609f02b2a7cbf302017eaebed843b34e8065ab
LuckyVAL06 <lopezvalentinoscar@gmail.com>
2025-04-29 21:21:09 -0300

    clase Conexion y variables SES

--- TP5_Grupo_8/AgregarSucursal.aspx.cs
+++ TP5_Grupo_8/AgregarSucursal.aspx.cs
@@ -9,9 +9,13 @@ namespace TP5_Grupo_8
 {
 	public partial class AgregarSucursal : System.Web.UI.Page
 	{
+		private const string cadenaConexion = @"Data Source=localhost\SQLEXPRESS;Initial Catalog = BDSucursales; Integrated Security = True; Encrypt=False";
+		private string consultaSQL;
+		private int filasAfectadas;
+		private Conexion conexion = new Conexion();
 		protected void Page_Load(object sender, EventArgs e)
 		{
-
+	
 		}
 	}
 }
---
+++ TP5_Grupo_8/Conexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TP5_Grupo_8
+{
+    public class Conexion
+    {
+        private const string cadenaConexion = @"Data Source=localhost\SQLEXPRESS;Initial Catalog = BDSucursales; Integrated Security = True; Encrypt=False";
+        int filasAfectadas;
+        public int EjecutarTransaccion(string consultaSQL)
+        {
+
+            return filasAfectadas;
+        }
+
+    }
+}
+
