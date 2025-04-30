321b754f5a358b9d059fb3c35a2a692b1999d6b2
Xhispas <acevedo.candela066@gmail.com>
2025-04-30 15:00:24 -0300

    SQLCOMMAND

--- TP5_Grupo_8/Conexion.cs
+++ TP5_Grupo_8/Conexion.cs
@@ -9,11 +9,14 @@ namespace TP5_Grupo_8
     public class Conexion
     {
         private const string cadenaConexion = @"Data Source=localhost\SQLEXPRESS;Initial Catalog = BDSucursales; Integrated Security = True; Encrypt=False";
+        string consultaSQL = " ";
         int filasAfectadas;
+
         public int EjecutarTransaccion(string consultaSQL)
         {
 
             SqlConnection sqlConexion = new SqlConnection(cadenaConexion);
+            SqlCommand SqlCommand = new SqlCommand (consultaSQL, sqlConexion);
             sqlConexion.Open();
 
             //ingresar codigo
