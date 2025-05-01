c73dcc1a85bc7d6dcb2fe816c30d83c3134c07a3
Xhispas <acevedo.candela066@gmail.com>
2025-04-30 23:33:49 -0300

    CONSULTA DE INSERT
    OWO

--- TP5_Grupo_8/AgregarSucursal.aspx.designer.cs
+++ TP5_Grupo_8/AgregarSucursal.aspx.designer.cs
@@ -148,5 +148,14 @@ namespace TP5_Grupo_8
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnAceptar;
+
+        /// <summary>
+        /// Control mensajeError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label mensajeError;
     }
 }
