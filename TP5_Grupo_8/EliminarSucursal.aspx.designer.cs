cfe3750bf577e82d8403b9e9dad2b6e3d1d1ed3b
Xhispas <acevedo.candela066@gmail.com>
2025-05-04 13:05:53 -0300

    Diseño owo
    (⊙_⊙)？(⊙_⊙)？(⊙_⊙)？(⊙_⊙)？(⊙_⊙)？＼（〇_ｏ）／＼（〇_ｏ）／(⊙_⊙)？＼（〇_ｏ）／＼（〇_ｏ）／(⊙_⊙)？(⊙_⊙)？(⊙ˍ⊙)(⊙ˍ⊙)(⊙_(⊙_⊙)_⊙)(⊙_(⊙_⊙)_⊙)(⊙_(⊙_⊙)_⊙)(⊙_(⊙_⊙)_⊙)

--- TP5_Grupo_8/EliminarSucursal.aspx.designer.cs
+++ TP5_Grupo_8/EliminarSucursal.aspx.designer.cs
@@ -40,5 +40,59 @@ namespace TP5_Grupo_8
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+
+        /// <summary>
+        /// Control HyperLink2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink2;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Control TxtSucursal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtSucursal;
+
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Control msjValidacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label msjValidacion;
     }
 }
